84bf420d6b642baae3fb57915d0def08451c8a40
Max Winde <max.winde@gmail.com>
2012-06-01 18:05:57 +0200

    something that looks a little bit like a website!

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+$().ready(function() {
+	if ((navigator.userAgent.match(/Chrome/)) || (navigator.userAgent.match(/Chromimum/))) {
+  	$('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', '/TabulaTabs.crx');
+	} else if ((navigator.userAgent.match(/Safari/)) && !navigator.userAgent.match(/Mobile/)) {
+  	$('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', '/TabulaTabs.safariextz');
+	}
+});
