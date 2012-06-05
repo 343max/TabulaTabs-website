1f9afee694032e6b07c1f3a5fa3062179496baf3
Max Winde <max.winde@gmail.com>
2012-06-05 14:23:27 +0200

    better visibility of compatibility notes on non supported browsers

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 
 $().ready(function() {
-	if ((navigator.userAgent.match(/Chrome/)) || (navigator.userAgent.match(/Chromimum/))) {
-  	$('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', '/TabulaTabs.crx');
-	} else if ((navigator.userAgent.match(/Safari/)) && !navigator.userAgent.match(/Mobile/)) {
-  	$('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', '/TabulaTabs.safariextz');
-	}
+    if ((navigator.userAgent.match(/Chrome/)) || (navigator.userAgent.match(/Chromimum/))) {
+        $('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', '/TabulaTabs.crx');
+    } else if ((navigator.userAgent.match(/Safari/)) && !navigator.userAgent.match(/Mobile/)) {
+        $('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', '/TabulaTabs.safariextz');
+    } else {
+        $('li.compatibility').addClass('important');
+    }
 });
