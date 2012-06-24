0c6333e0c2387cb01c3ee001ba7a5f2bfa8a65e0
Max Winde <max.winde@gmail.com>
2012-06-24 13:45:51 +0200

    chrome extension from the google webstore

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 $().ready(function() {
     if ((navigator.userAgent.match(/Chrome/)) || (navigator.userAgent.match(/Chromimum/))) {
-        $('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', '/TabulaTabs.crx');
+        $('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', 'https://chrome.google.com/webstore/detail/dckibfapbkkgkmipdkjbpdcopejindph');
     } else if ((navigator.userAgent.match(/Safari/)) && !navigator.userAgent.match(/Mobile/)) {
         $('.browser-extension').addClass('btn-inverse').removeClass('disabled').attr('href', '/TabulaTabs.safariextz');
     } else {
